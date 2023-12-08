56edde0d6f5e7284fb6798718709e9f448cec88d
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2023-12-08 09:32:16 +0100

    feat(main): solution for task 5

--- main.c
+++ main.c
@@ -180,8 +180,198 @@ void task4(){
     }
 
     printf("\n\n");
+}
+
+void task5(){
+
+    printf("Aufgabe 5: Tic Tac Toe\n\n");
+
+    printf("Willkommen bei Tic Tac Toe\n\nSpieler 1:\tO\nSpieler 2:\tX\n\n");
+
+    int currentPlayer = 1;
+    int winner = -1;
+    int field[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
+    int coords[2] = {0,0};
+    int playerTurn[2]={0,0};
+
+    while(winner == -1){
+        printf("Spieler %d ist am Zug.", currentPlayer);
+
+        int validX;
+        do {
+            validX = 1;
+            printf("\n\nBitte geben Sie die X-Koordinate ihres Zuges ein.\n\n");
+
+            scanf("%d", &coords[0]);
+
+            if(coords[0] > 3 || coords[0] < 0){
+                printf("\nIhre Eingabe ist ungueltig. Bitte versuchen Sie es erneut. Der Wert muss 1, 2 oder 3 sein.\n");
+                validX = 0;
+            }
+
+            int hasFreeXSpot = 0;
+
+            for(int i = 0; i < 3; i++){
+                if(field[coords[0]-1][i] == 0){
+                    hasFreeXSpot = 1;
+                    i = 3;
+                }
+            }
+
+            if(hasFreeXSpot != 1){
+                printf("\nIn dieser Spalte gibt es keinen freien Platz mehr. Wählen Sie einen anderen Wert.\n");
+                validX = 0;
+            }
 
 
+        } while(validX != 1);
+
+        printf("\nDie gewaehlte X-Koordinate ist: %d\n\n", coords[0]);
+
+        int validY;
+        do {
+            validY = 1;
+            printf("\n\nBitte geben Sie die Y-Koordinate ihres Zuges ein.\n\n");
+
+            scanf("%d", &coords[1]);
+
+            if(coords[1] > 3 || coords[1] < 0){
+                printf("\nIhre Eingabe ist ungueltig. Bitte versuchen Sie es erneut. Der Wert muss 1, 2 oder 3 sein.\n");
+                validY = 0;
+            }
+
+            int hasFreeYSpot = 0;
+
+            for(int i = 0; i < 3; i++){
+                if(field[i][coords[1]] == 0){
+                    hasFreeYSpot = 1;
+                    i = 3;
+                }
+            }
+
+            if(hasFreeYSpot != 1){
+                validY = 0;
+            }
+
+            if(field[coords[1]-1][coords[0]-1] != 0){
+                printf("\nDieses Feld ist bereits belegt. Bitte verwenden Sie einen anderen Y-Wert.");
+                validY = 0;
+            }
+
+        } while(validY != 1);
+
+        printf("\nDie gewaehlte Y-Koordinate ist: %d\n\nSpieler %d setzt eine Markierung auf %d/%d\n\nDas Spielfeld sieht nun wie folgt aus:\n\n", coords[1],currentPlayer,coords[0],coords[1]);
+
+
+        field[coords[1]-1][coords[0]-1] = currentPlayer;
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(field[i][j] == 1){
+                    printf(" O ");
+                } else if (field[i][j] == 2){
+                    printf(" X ");
+                } else {
+                    printf("   ");
+                }
+                if(j < 2){
+                    printf("|");
+                }
+            }
+            if(i < 2){
+                printf("\n-----------\n");
+            } else {
+                printf("\n");
+            }
+
+        }
+
+
+        playerTurn[currentPlayer-1]++;
+
+
+        printf("\n--------------------------------------------------------\n");
+
+        if(playerTurn[0]+playerTurn[1] == 9){
+            printf("\nDas Spiel ist vorbei. Ergebnis: Unentschieden.");
+            winner = 0;
+        }
+
+        //Check winning conditions
+
+        //horizontal
+        for(int i = 0; i < 3; i++){
+            int counter = 0;
+            for(int j = 0; j < 3; j++){
+                if(field[i][j] == currentPlayer){
+                    counter++;
+                }
+
+            }
+
+            if(counter == 3){
+                winner = currentPlayer;
+                printf("\nDas Spiel ist vorbei. Spieler %d hat durch eine horizontale Linie gewonnen!\n", winner);
+            } else {
+                counter = 0;
+            }
+        }
+
+        //vertical
+        for(int i = 0; i < 3; i++){
+            int counter = 0;
+            for(int j = 0; j < 3; j++){
+                if(field[j][i] == currentPlayer){
+                    counter++;
+                }
+
+            }
+
+            if(counter == 3){
+                winner = currentPlayer;
+                printf("\nDas Spiel ist vorbei. Spieler %d hat durch eine vertikale Linie gewonnen!\n", winner);
+            } else {
+                counter = 0;
+            }
+        }
+
+        //diagonal
+        //top left - bottom right
+        int diagonalCounter = 0;
+        for(int i = 0; i < 3; i++){
+            if(field[i][i]==currentPlayer){
+                diagonalCounter++;
+            }
+
+        }
+
+
+        if(diagonalCounter == 3){
+            winner = currentPlayer;
+            printf("\nDas Spiel ist vorbei. Spieler %d hat durch eine Diagonale Linie von oben links nach unten rechts gewonnen!\n", winner);
+        }
+
+        diagonalCounter = 0;
+
+        for(int i = 2; i > -1; i--){
+            for(int j = 0; j < 3; j++){
+                if(field[i][j] == currentPlayer){
+                    diagonalCounter++;
+                }
+            }
+        }
+
+        if(diagonalCounter == 3){
+            winner = currentPlayer;
+            printf("\nDas Spiel ist vorbei. Spieler %d hat durch eine Diagonale Linie von oben rechts nach unten links gewonnen!\n", winner);
+        }
+
+        if(currentPlayer == 1){
+            currentPlayer = 2;
+        } else {
+            currentPlayer = 1;
+        }
+    }
 }
 
 int main()
@@ -189,6 +379,7 @@ int main()
     task2();
     task3();
     task4();
+    task5();
 
     return 0;
 }
